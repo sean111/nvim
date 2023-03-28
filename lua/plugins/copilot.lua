95d9e42fbc9c197d677189ce342956b6d179e9ad
Sean Burke <sean111@gmail.com>
2023-03-28 01:15:03 -0400

    Added copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+    "zbirenbaum/copilot-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        config = function()
+          require("copilot").setup({})
+        end,
+      },
+      "VonHeikemen/lsp-zero.nvim",
+    },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  }
