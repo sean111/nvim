34572b4b97391d183fb5e561909d3c2ca5d6f6df
Sean Burke <sean111@gmail.com>
2023-03-29 13:20:38 +0000

    Added fine-cmd, mini.animate and messing arount with colorscheme stuff a tad

--- lua/plugins/tundra.lua
+++ lua/plugins/tundra.lua
@@ -1,31 +1,31 @@
 return {
-    --     {
-    --         "sam4llis/nvim-tundra",
-    --         config = function()
-    --             require('nvim-tundra').setup({
-    --                 plugins = {
-    --                     lsp = true,
-    --                     treesitter = true,
-    --                     cmp = true,
-    --                     context = true,
-    --                     dbui = false,
-    --                     gitsigns = true,
-    --                     telescope = true
-    --                 },
-    --                 syntax = {
-    --                     booleans = { bold = true, italic = true },
-    --                     comments = { italic = true },
-    --                     types = { italic = true },
-    --                     constants = { bold = true }
-    --                 }
-    --             })
-    --         end,
-    --         enabled = false,
-    --     },
-    --     {
-    --         "LazyVim/LazyVim",
-    --         opts = {
-    --             colorscheme = "tundra",
-    --         },
-    --     },
+        {
+            "sam4llis/nvim-tundra",
+            config = function()
+                require('nvim-tundra').setup({
+                    plugins = {
+                        lsp = true,
+                        treesitter = true,
+                        cmp = true,
+                        context = true,
+                        dbui = false,
+                        gitsigns = true,
+                        telescope = true
+                    },
+                    syntax = {
+                        booleans = { bold = true, italic = true },
+                        comments = { italic = true },
+                        types = { italic = true },
+                        constants = { bold = true }
+                    }
+                })
+            end,
+            enabled = false,
+        },
+        -- {
+        --     "LazyVim/LazyVim",
+        --     opts = {
+        --         colorscheme = "tundra",
+        --     },
+        -- },
 }
